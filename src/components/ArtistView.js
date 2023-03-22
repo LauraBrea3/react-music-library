1e93341e171c93741b577c232266366b63726a47
Laura Brea <laurabrea17@gmail.com>
2023-03-22 14:27:25 -0400

    installed router package and rendered new components to app.js

---
+++ src/components/ArtistView.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+export default function ArtistView() {
+    const [ artistData, setArtistData ] = useState([])
+
+
+  return (
+    <div>
+        <p>Artist Data Goes Here!</p>
+    </div>
+  )
+}
